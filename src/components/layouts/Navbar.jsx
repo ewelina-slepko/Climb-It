1a80e4c1d8e93ea4c9fa3843f6337ce588ae9624
ewelina.slepko <slepko.ewelina@gmail.com>
2019-06-25 23:19:01 +0200

    Created App Bar with Material UI

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+const Navbar = () => (
+    <nav className="nav-wrapper">
+        <div className="container">
+            <Link to='/'>Link</Link>
+        </div>
+    </nav>
+)
 
-const Navbar = () => {
-
-}
+export default Navbar;
