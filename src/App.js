4a0e7d8b282d4b49ef5b3fd3b8279583ae40efdf
ewelina.slepko <slepko.ewelina@gmail.com>
2019-06-27 20:10:33 +0200

    Added ButtonAppBar component (material UI)

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/layouts/Navbar';
+import ButtonAppBar from './components/layouts/ButtonAppBar';
 
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <ButtonAppBar />
+      </div>
+    </BrowserRouter>
+  );
+}
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-  const classes = useStyles();
-
-  function App() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" className={classes.title}>
-                News
-          </Typography>
-              <Button color="inherit">Login</Button>
-            </Toolbar>
-          </AppBar>
-          <Navbar></Navbar>
-        </div>
-      </BrowserRouter>
-    );
-  }
-
-  export default App;
+export default App;
