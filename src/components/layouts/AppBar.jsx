a3758ec2ef406df2ab7d877fbe884c2121c5711f
ewelina.slepko <slepko.ewelina@gmail.com>
2019-06-29 14:33:36 +0200

    closing Drawer after click on list item

--- src/components/layouts/AppBar.jsx
+++ src/components/layouts/AppBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import Dashboard from '../dashboard/Dashboard';
 import SignedInLinks from './SignedInLinks';
 
 import clsx from 'clsx';
@@ -29,47 +30,39 @@ const PersistentDrawerLeft = () => {
                 position="fixed"
                 className={clsx(classes.appBar, {
                     [classes.appBarShift]: open,
-                })}
-            >
+                })}>
                 <Toolbar>
                     <IconButton
                         color="inherit"
                         aria-label="Open drawer"
                         onClick={handleDrawerOpen}
                         edge="start"
-                        className={clsx(classes.menuButton, open && classes.hide)}
-                    >
+                        className={clsx(classes.menuButton, open && classes.hide)}>
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" noWrap>
-                        <Link to='/' className={classes.link}>Home</Link>
+                        <Link to='/' className={classes.link}><Dashboard /></Link>
                     </Typography>
                 </Toolbar>
             </AppBar>
             <Drawer
-                className={classes.drawer}
                 variant="persistent"
                 anchor="left"
                 open={open}
                 classes={{
                     paper: classes.drawerPaper,
-                }}
-            >
-                <div className={classes.drawerHeader}>
+                }}>
+                <div>
                     <IconButton onClick={handleDrawerClose}>
                         {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
                     </IconButton>
                 </div>
                 <Divider />
-                <List>
+                <List onClick={handleDrawerClose}>
                     <SignedInLinks />
                 </List>
             </Drawer>
-            <main
-                className={clsx(classes.content, {
-                    [classes.contentShift]: open,
-                })}
-            >
+            <main className={classes.content}>
                 <div className={classes.drawerHeader} />
             </main>
         </div>
@@ -82,12 +75,6 @@ const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
     },
-    appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
     appBarShift: {
         width: `calc(100% - ${drawerWidth}px)`,
         marginLeft: drawerWidth,
@@ -96,46 +83,20 @@ const useStyles = makeStyles(theme => ({
             duration: theme.transitions.duration.enteringScreen,
         }),
     },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
     hide: {
         display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
     },
     drawerPaper: {
         width: drawerWidth,
     },
-    drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        padding: '0 8px',
-        ...theme.mixins.toolbar,
-        justifyContent: 'flex-end',
-    },
     content: {
         flexGrow: 1,
-        padding: theme.spacing(3),
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginLeft: -drawerWidth,
-    },
-    contentShift: {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
+        padding: '40px'
     },
     link: {
         textDecoration: 'none',
         color: '#fff',
-        textTransform: 'upperCase'
+        textTransform: 'upperCase',
     }
 }));
 
--- src/components/layouts/SignedInLinks.jsx
+++ src/components/layouts/SignedInLinks.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
-import { makeStyles } from '@material-ui/core/styles';
 
 const arr = [
     { text: 'Chart', path: '/' },
@@ -12,18 +11,17 @@ const arr = [
 ];
 
 const SignedInLinks = () => {
-    const classes = useStyles();
     return (
         <>
             {arr.map((obj, index) => (
-                <ListItem component={NavLink} to={obj.path} className={classes.link} button key={index}>
+                <ListItem component={Link} to={obj.path} button key={index}>
                     <ListItemText>
                         {obj.text}
                     </ListItemText>
                 </ListItem>
             ))}
             <Divider />
-            <ListItem component={NavLink} to='/logout' className={classes.link} button key='logout'>
+            <ListItem component={Link} to='/logout' button key='logout'>
                 <ListItemText>
                     Log out
                 </ListItemText>
@@ -31,12 +29,5 @@ const SignedInLinks = () => {
         </>
     );
 }
-
-const useStyles = makeStyles(theme => ({
-    link: {
-        textDecoration: 'none',
-        color: '#000',
-    }
-}));
 
 export default SignedInLinks;
