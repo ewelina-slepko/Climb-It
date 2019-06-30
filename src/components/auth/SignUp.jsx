de97544f710349b7f927a63bc1bc0dce86cfbf5c
ewelina.slepko <slepko.ewelina@gmail.com>
2019-06-30 22:18:39 +0200

    Created SignUp Component

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,14 +1,101 @@
-// import React from 'react'
+import React from 'react'
 
-// class SignUp extends React.Component {
-//     state = {
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-//     }
-//     render() {
-//         return (
+const SignUp = () => {
+    const classes = useStyles();
+    const [values, setValues] = React.useState({
+        name: '',
+        email: '',
+        emailConfirmed: '',
+        password: ''
+    });
 
-//         )
-//     }
-// }
+    const handleChange = name => event => {
+        event.preventDefault();
+        setValues({ ...values, [name]: event.target.value });
+        console.log(values)
+    };
 
-// export default SignUp;
+    return (
+        <div className={classes.container}>
+            <form noValidate autoComplete="off" onSubmit={e => e.preventDefault() || alert(JSON.stringify(values))}>
+                <TextField
+                    required
+                    id="standard-name"
+                    label="Login"
+                    className={classes.textField}
+                    margin="normal"
+                    value={values.name}
+                    onChange={handleChange('name')}
+                />
+                <TextField
+                    required
+                    id="standard-name"
+                    label="E-mail"
+                    className={classes.textField}
+                    margin="normal"
+                    value={values.email}
+                    onChange={handleChange('email')}
+                />
+                <TextField
+                    required
+                    id="standard-name"
+                    label="Potwierdź adres e-mail"
+                    className={classes.textField}
+                    margin="normal"
+                    value={values.emailConfirmed}
+                    onChange={handleChange('emailConfirmed')}
+                />
+                <TextField
+                    required
+                    id="standard-password-input"
+                    label="Password"
+                    className={classes.textField}
+                    type="password"
+                    autoComplete="current-password"
+                    margin="normal"
+                    value={values.password}
+                    onChange={handleChange('password')}
+                />
+                <Button variant="contained" color="primary" className={classes.button} type="submit" label="Login" onSubmit={handleChange('name', 'password')}>
+                    Sign Up
+                </Button>
+            </form>
+        </div>
+
+    )
+}
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        margin: '0 auto',
+        display: 'flex',
+        width: 300,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 100,
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: 250,
+    },
+    dense: {
+        marginTop: 16,
+    },
+    menu: {
+        width: 200,
+    },
+    button: {
+        margin: '50px 0 0 6px',
+    },
+    input: {
+        display: 'none',
+    },
+}));
+
+export default SignUp;
--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import CreateNewRoute from '../ClimbingRoutes/CreateNewRoute';
 import RoutesList from '../ClimbingRoutes/RouteList';
 import RouteDetails from '../ClimbingRoutes/RouteDetails';
 import SignIn from '../auth/SignIn';
+import SignUp from '../auth/SignUp';
 
 class Dashboard extends React.Component {
     render() {
@@ -15,6 +16,7 @@ class Dashboard extends React.Component {
                 <Route path="/listofroutes" component={RoutesList} />
                 <Route path="/project/:id" component={RouteDetails} />
                 <Route path="/signin/" component={SignIn} />
+                <Route path="/signup/" component={SignUp} />
             </Switch>
         )
     }
