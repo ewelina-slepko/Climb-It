230ed1a04ec39cd8b0058847827bfbf3cf199029
ewelina.slepko <slepko.ewelina@gmail.com>
2019-07-02 20:55:35 +0200

    Created a redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
