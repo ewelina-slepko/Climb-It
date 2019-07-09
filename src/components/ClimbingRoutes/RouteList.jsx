88442b83a1a26cc297b2d65624fe402bfb4b87d8
ewelina.slepko <slepko.ewelina@gmail.com>
2019-07-09 21:40:19 +0200

    Created detail panel in RouteList Component

--- src/components/ClimbingRoutes/RouteList.jsx
+++ src/components/ClimbingRoutes/RouteList.jsx
@@ -1,36 +1,33 @@
 import React from 'react';
 import MaterialTable from 'material-table';
-import { fontStyle } from '@material-ui/system';
+import { Link } from 'react-router-dom';
 
 
 const RoutesList = ({ projects }) => {
     const [state, setState] = React.useState({
         columns: [
             { title: 'Date', field: 'date' },
-            // { title: 'Location', field: 'location' },
-            // { title: 'Rock name', field: 'rockName' },
             { title: 'Route name', field: 'routeName' },
-            // { title: 'Climbing style', field: 'climbingStyle' },
-            // { title: 'Grading system', field: 'gradingSystem' },
             { title: 'Difficulty', field: 'difficulty' },
-            // { title: 'Description', field: 'description' },
         ],
         data: projects.map(project => {
             return {
-                date: <p>{project.date}</p>,
-                // location: <h2>{project.location}</h2>,
-                // rockName: <p>{project.rockName}</p>,
-                routeName: <p>{project.routeName}</p>,
-                // climbingStyle: <p>{project.climbingStyle}</p>,
-                // gradingSystem: <p>{project.gradingSystem}</p>,
-                difficulty: <p>{project.difficulty}</p>,
-                // description: <p>{project.description}</p>
+                date: <span>{project.date}</span>,
+                location: <span>{project.location}</span>,
+                rockName: <span>{project.rockName}</span>,
+                routeName: <span>{project.routeName}</span>,
+                climbingStyle: <span>{project.climbingStyle}</span>,
+                gradingSystem: <span>{project.gradingSystem}</span>,
+                difficulty: <span>{project.difficulty}</span>,
+                description: <span>{project.description}</span>
             }
         }),
     });
     return (
         <>
             <MaterialTable
+                onCellClick={() => { alert("Table Row Clicked!! ") }}
+                component={Link} to="/home"
                 title="List of Routes"
                 columns={state.columns}
                 data={state.data}
@@ -56,11 +53,48 @@ const RoutesList = ({ projects }) => {
                     //     }),
 
                 }}
-
+                detailPanel={[
+                    {
+                        icon: 'chevron_right',
+                        tooltip: 'Show Details',
+                        render: rowData => {
+                            return (
+                                <div
+                                    style={{
+                                        fontSize: 16,
+                                        backgroundColor: '#fff',
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        alignItem: 'center',
+                                        justifyContent: 'center',
+                                        margin: 40
+                                    }}
+                                >
+                                    <ul
+                                    >
+                                        <li style={styles} >Date: {rowData.date}</li>
+                                        <li style={styles}>Location: {rowData.location}</li>
+                                        <li style={styles}>Rock name: {rowData.rockName}</li>
+                                        <li style={styles}>Route name: {rowData.routeName}</li>
+                                        <li style={styles}>Climbing style: {rowData.climbingStyle}</li>
+                                        <li style={styles}>Grading system: {rowData.gradingSystem}</li>
+                                        <li style={styles}>Difficulty: {rowData.difficulty}</li>
+                                        <li style={styles}>Description: {rowData.description}</li>
+                                    </ul>
+                                </div>
+                            )
+                        },
+                    },
+                ]}
             />
         </>
     )
 }
 
+const styles = {
+    listStyleType: 'none',
+    padding: 3,
+    color: '#575757'
+}
 
 export default RoutesList;
