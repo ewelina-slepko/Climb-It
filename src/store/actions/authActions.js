ab74d8da566c139737990cfc544c8f3b2e590da4
ewelina.slepko <slepko.ewelina@gmail.com>
2019-07-10 20:59:48 +0200

    Added auth reducer

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -7,7 +7,7 @@ export const signIn = (credentials) => {
         ).then(() => {
             dispatch({ type: 'LOGIN_SUCCESS' })
         }).catch((err) => {
-            dispatch({ type: 'LOGIN_ERROR' })
+            dispatch({ type: 'LOGIN_ERROR', err })
         })
     }
 }
